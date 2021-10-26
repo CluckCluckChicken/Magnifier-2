9365223d4ab5859dd15cfc9f1e99589a900150a1
potatophant <46281840+CluckCluckChicken@users.noreply.github.com>
2021-10-26 18:06:06 +0100

    Update Constants.cs

--- Shared/Models/Universal/Constants.cs
+++ Shared/Models/Universal/Constants.cs
@@ -13,7 +13,7 @@ namespace Shared.Models.Universal
 
         public static readonly Uri authProject = new Uri("https://api.scratch.mit.edu/users/furrycat-auth/projects/534514916/comments");
 
-        public static readonly Regex URL_REGEX = new Regex("/(\b(https?):\\/\\/[-A-Z0-9+&@#/%?=~_|!:,.;]*scratch\\.mit\\.edu[-A-Z0-9+&@#/%=~_|]*)/ig");
-        public static readonly Regex MENTION_REGEX = new Regex("/(?<=(^|[\\s.,!])(?!\\S*[:#]))[@|＠][a-z0-9_-]{1,20}(?=(?:\b(?!@|＠)|$))/ig");
+        public static readonly Regex URL_REGEX = new Regex("/(\b(https?):\\/\\/[-A-Z0-9+&@#/%?=~_|!:,.;]*scratch\\.mit\\.edu[-A-Z0-9+&@#/%=~_|]*)/ig", RegexOptions.IgnoreCase);
+        public static readonly Regex MENTION_REGEX = new Regex("(?<=(^|[\s.,!])(?!\S*[:#]))[@|＠][A-Za-z0-9_-]{1,20}(?=(?:\b(?!@|＠)|$))", RegexOptions.IgnoreCase);
     }
 }
