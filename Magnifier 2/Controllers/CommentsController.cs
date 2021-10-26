2241200df49a95a2c27c477581b3bcc4bb6782fd
potatophant <46281840+CluckCluckChicken@users.noreply.github.com>
2021-10-26 18:08:30 +0100

    Update CommentsController.cs

--- Magnifier 2/Controllers/CommentsController.cs
+++ Magnifier 2/Controllers/CommentsController.cs
@@ -48,13 +48,13 @@ namespace Magnifier_2.Controllers
 
             foreach (Comment comment in comments)
             {
-                comment.Content = Constants.MENTION_REGEX.Replace(comment.Content, mention => $"<a href=\"https://scratch.mit.edu/users/${mention.Value.Substring(1)}\">{mention}</a>");
+                comment.Content = Constants.MENTION_REGEX.Replace(comment.Content, mention => $"<a href=\"/users/${mention.Value.Substring(1)}\">{mention}</a>");
 
                 comment.Replies = JsonSerializer.Deserialize<List<Comment>>(await client.GetStringAsync($"{url}/{comment.CommentId}/replies"));
 
                 foreach (Comment reply in comment.Replies)
                 {
-                    reply.Content = Constants.MENTION_REGEX.Replace(reply.Content, mention => $"<a href=\"https://scratch.mit.edu/users/${mention.Value.Substring(1)}\">{mention}</a>");
+                    reply.Content = Constants.MENTION_REGEX.Replace(reply.Content, mention => $"<a href=\"/users/${mention.Value.Substring(1)}\">{mention}</a>");
                 }
             }
 
